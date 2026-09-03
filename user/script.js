5e241a850d31b630bf37d6b997d1d9fcbd4cde1e
timothyolivieri <timothyolivieri@outlook.com>
2026-09-03 09:22:44 -0400

    Update script.js

--- user/script.js
+++ user/script.js
@@ -1,3 +1,7 @@
+window.mvc = {};
+mvc.model = {};
+mvc.view = {};
+mvc.controller = {};
 document.addEventListener("DOMContentLoaded", () => {
   var githubToken = localStorage.getItem("github-token");
   if(githubToken) {
