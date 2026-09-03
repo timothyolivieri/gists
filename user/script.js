50ce0490ce4284970ed70a361940ad5c71645f05
timothyolivieri <timothyolivieri@outlook.com>
2026-09-03 09:38:27 -0400

    Update script.js

--- user/script.js
+++ user/script.js
@@ -4,7 +4,9 @@ mvc.view = {};
 mvc.controller = {};
 mvc.controller.token = (event) => {
   event.preventDefault();
-  alert("Checking token...");
+  var token = event.target.querySelector('input[type="password"]').value;
+  alert("Checking token... " + token);
+  localStorage.setItem('github-token', token);
 };
 document.addEventListener("DOMContentLoaded", () => {
   var githubToken = localStorage.getItem("github-token");
