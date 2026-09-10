88faf6dd0f116b278698f765c863404080a6c510
timothyolivieri <timothyolivieri@outlook.com>
2026-09-10 09:19:07 -0400

    Update script.js

--- user/script.js
+++ user/script.js
@@ -1,17 +1,26 @@
 window.mvc = {};
 mvc.model = {};
 mvc.view = {};
+mvc.view['/user'] = () => {
+  console.log('Getting My Gists');
+}
 mvc.controller = {};
+mvc.controller.logout = () => {
+  localStorage.removeItem('github-token');
+  document.body.removeAttribute('auth');
+}
 mvc.controller.token = (event) => {
   event.preventDefault();
   var githubToken = event.target.querySelector('input[type="password"]').value;
   alert("Checking token... " + githubToken);
   localStorage.setItem('github-token', githubToken);
-  document.body.setAttribute("auth", githubToken); 
+  document.body.setAttribute("auth", githubToken);
+  mvc.view['/user']();
 };
 document.addEventListener("DOMContentLoaded", () => {
   var githubToken = localStorage.getItem("github-token");
   if(githubToken) {
-    document.body.setAttribute("auth", githubToken);    
+    document.body.setAttribute("auth", githubToken); 
+    mvc.view['/user']();   
   }
 });
